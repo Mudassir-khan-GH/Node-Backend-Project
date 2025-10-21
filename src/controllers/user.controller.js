d2a6476a1a3f442198083c6ee36912f1bb7d6d09
Mudassir <mudassirpak47#gmail.com>
2025-10-21 23:26:57 +0500

    feat: enhance home page layout and add debug logging for data retrieval

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -334,5 +334,8 @@ exports.getHomePage = async (req, res) => {
             }
         }
     ])
+    console.log(dataForHome);
+    console.log(dataForHome[0]);
+    
     res.render("home", { user: req?.user , allPosts: dataForHome})
 }
